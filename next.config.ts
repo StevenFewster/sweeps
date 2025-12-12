3509d74ef8ebea2f73b17057d28d0f08679965bd
Steven Fewster <steven.fewster@gmail.com>
2025-12-12 20:55:22 +0000

    Update basetPath

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/static-sweep' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/sweeps' : '',
 };
 
 export default nextConfig;
